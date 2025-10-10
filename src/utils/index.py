9d7ad457bf95ce0c00dd4efc7b49bc292d726178
Marcelo <marcellus.costa@outlook.com>
2025-10-09 23:13:20 -0300

    novas funções de cálculo de carbono

--- src/utils/index.py
+++ src/utils/index.py
@@ -14,16 +14,31 @@ def carregajson(file):
     else:
         print("fatores nãoo declarados")
 
-carregajson(file)
+# Tratamento de dados
+def trata_numero(valor):
+    try:
+        valor = float(valor)
+        if valor < 0:
+            raise ValueError("O valor não pode ser negativo.")
+        return valor
+    except ValueError:
+        raise ValueError("Entrada inválida. Por favor, insira um número válido.")
 
+# Calculo de emissões
+def calc_eletricidade(consumo_mensal):
+    return consumo_mensal * carregajson(file)["eletricidade/kwh"]
 
+def calc_combustivel(consumo_mensal):
+    return consumo_mensal * carregajson(file)["combustivel/litro"]
 
+def calc_voo(distancia):
+    return distancia * carregajson(file)["voo/km"]
 
+def calc_credito(entradas):
+    total_emissao = 0
 
+    total_emissao += calc_eletricidade(entradas['eletricidade'])
+    total_emissao += calc_combustivel(entradas['combustivel'])
+    total_emissao += calc_voo(entradas['voo'])
 
-
-
-
-
-
-
+    return total_emissao / carregajson(file)["credito/ton"]
